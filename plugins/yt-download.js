0c3ad92e1c47900c52a5cfb3bd671eee64ab07b8
Trilokhw🌷 <pranavxyy@gmail.com>
2024-08-17 23:54:57 +0530

    yt-download.js

--- plugins/yt-download.js
+++ plugins/yt-download.js
@@ -1,6 +1,8 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
-const yts
+const yts = require('yt-search')
+
+
 cmd({
     pattern: "song",
     desc: "download songs",
@@ -9,6 +11,23 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+if(!q) return reply("> *ɢɪᴠᴇ ᴍᴇ ᴜʀʟ !!*")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let dasc = `
+> *ɪᴋᴜᴢᴇ 🧚🏻*
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});    q
+
+//download audio
+
+let down =await fg.yta(url)
+let downloadUrl =down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})    
 
 
 
